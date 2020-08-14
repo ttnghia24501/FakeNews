71b4e0e18b2e0eabab64f52c367980f7c547a2c5
ttnghia24501 <53210685+ttnghia24501@users.noreply.github.com>
2020-08-14 10:04:10 +0700

    Add project files.

---
+++ Model/NewsItem.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeNews.Model
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Headline { get; set; }
+        public string Subhead { get; set; }
+        public string DateLine { get; set; }
+        public string Image { get; set; }
+    }
+    
+    public class NewsManager
+    {
+        private static List<NewsItem> getNewsItems()
+        {
+            var items = new List<NewsItem>();
+
+            items.Add(new NewsItem()
+            {
+                Id = 1,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Financial.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 2,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Financial2.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 3,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Financial3.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 4,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Financial4.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 5,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Financial5.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 6,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Food1.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 7,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Food2.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 8,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Food3.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 9,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Food4.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 10,
+                Category = "Financial",
+                Headline = "Lorem Ipsum",
+                Subhead = "doro sit amet",
+                DateLine = "Nunc stristque nec",
+                Image = "Assets/Food5.png"
+            });
+            return items;
+        }
+        public static void GetNews(string category, ObservableCollection<NewsItem> newsItems)
+        {
+            var allItems = getNewsItems();
+            var filteredNewsItems = allItems.Where(p => p.Category == category).ToList();
+            newsItems.Clear();
+            filteredNewsItems.ForEach(p => newsItems.Add(p));
+        }
+    }
+}
